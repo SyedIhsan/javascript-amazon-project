20d15be83d14d5a705133a944cbc5d53bb8d9edd
SyedIhsan <syedihsan1998@gmail.com>
2025-12-18 20:07:20 +0800

    Create automated tests for formatCurrency

---
+++ javascript-amazon-project-main/tests/moneyTest.js
@@ -0,0 +1,42 @@
+import formatCurrency from "../scripts/utils/money.js";
+
+console.log("test suite: formatCurrency");
+
+console.log("converts cents into dollars");
+// test cases
+if (formatCurrency(2095) === "20.95") {
+    console.log("passed");
+} else {
+    console.log("failed")
+};
+
+console.log("works with 0");
+// edge cases
+if (formatCurrency(0) === "0.00") {
+    console.log("passed");
+} else {
+    console.log("failed")
+};
+
+console.log("rounds up to the nearest cent");
+// edge cases
+if (formatCurrency(2000.5) === "20.01") {
+    console.log("passed");
+} else {
+    console.log("failed")
+};
+
+console.log("rounds down to the nearest cent");
+// edge cases
+if (formatCurrency(2000.4) === "20.00") {
+    console.log("passed");
+} else {
+    console.log("failed")
+};
+
+/*
+2 Types of Test Cases:
+
+1. Basic test cases = test if the code is working
+2. Edge cases = test with values that are tricky
+*/
